fb7511c4dbe7f20c03c8e9976eceaf4db38e2238
bmastroviti <bmastroviti@stsaviour.org>
2025-11-07 10:55:07 -0500

    done

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import random
 import time
 
+from player import Player
 from draw import draw_d20, draw_d6, draw_d4
 
 def print_dramatic_text(text: str, delay=0.1):
@@ -16,12 +17,29 @@ if __name__ == '__main__':
     # any buffs / debuffs?
     # any critical success / failure?
 
-    name = input('Name: ')
+    # player_1 = Player('Bianca', 'Doctor', 10)
+    # player_1.attack() THIS WAS ALL MEEE
+
+    # player_2 = Player('Miranda', 'Barbarian', 20)
+    # player_2.attack() THIS WAS ALL MEEE
+
+    input('Press enter to roll a 6-sided die. ')
+    roll = random.randint(1, 6)
+    name = ''
+    if roll == 1 or roll == 2:
+        name = 'Mr. Mint'
+    if roll == 3 or roll == 4:
+        name = 'Ms. Candy Cane'
+
+    player = Player(name, )
+
+
     role = input('Role: ')
 
-    print('Your name is ' + name + ' and your role is ' + role + '.')
-    print_dramatic_text('Our adventure begins in a shady tavern ...')
+    # player = Player(name, role, 10) THIS WAS MEEE
+    # print('Your name is ' + player.name + ' and your role is ' + player.role + '.') #  I ADDED TO THIS
+    # print_dramatic_text('Our adventure begins in a shady tavern ...')
 
-    input('Press Enter to roll a d20.')
-    roll = random.randint(1, 20)
-    draw_d20(roll)
+    # input('Press Enter to roll a d20.')
+    # roll = random.randint(1, 20)
+    # draw_d20(roll)
---
+++ player.py
@@ -0,0 +1,6 @@
+class Player: 
+    def __init__(self, name: str, role: str):
+        self.name = name
+        self.role = role
+
+    
