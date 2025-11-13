0c7ec0b4e72e7ca8f62d5c7daca12ce902396265
bmastroviti <bmastroviti@stsaviour.org>
2025-11-13 10:47:43 -0500

    hi

--- game.py
+++ game.py
@@ -23,18 +23,26 @@ if __name__ == '__main__':
     # player_2 = Player('Miranda', 'Barbarian', 20)
     # player_2.attack() THIS WAS ALL MEEE
 
-    input('Press enter to roll a 6-sided die. ')
-    roll = random.randint(1, 6)
-    name = ''
-    if roll == 1 or roll == 2:
-        name = 'Mr. Mint'
-    if roll == 3 or roll == 4:
-        name = 'Ms. Candy Cane'
+    # input('Press enter to roll a 6-sided die. ')
+    # roll = random.randint(1, 7)
+    # name = ''
+    # if roll == 1 or roll == 2:
+    #     name = 'Mr. Mint' or name = 'Princess Frosting'
+    # if roll == 3 or roll == 4:
+    #     name = 'Ms. Candy Cane' or name = 'Gloppy'
+    # if roll == 4 or roll == 6:
+    #     name = 'Lord Locorice'
+    # if roll == 7 or roll == 8:
+    #     name = 'Princess Lolly'
+        
+    print_dramatic_text("Welcome to Candy Land! Today you will have to compete with your opponet to get to King Kandy first.")
+    print_dramatic_text("First you will have to roll to pick your character.")
+    print_dramatic_text("The random character generator will give you the character")
 
     player = Player(name, )
 
 
-    role = input('Role: ')
+    role = input('Role:')
 
     # player = Player(name, role, 10) THIS WAS MEEE
     # print('Your name is ' + player.name + ' and your role is ' + player.role + '.') #  I ADDED TO THIS
